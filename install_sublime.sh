677d3d3598f7281e34e762d72927705ae08f8f65
jx2git <jx2git@gmail.com>
2022-05-02 06:18:03 -0400

    intial commit

---
+++ install_sublime.sh
@@ -0,0 +1,22 @@
+################################################################################################################
+############################################ Install Sublime Text ##############################################
+################################################################################################################
+
+# Install the GPG key:
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+#sudo wget https://download.sublimetext.com/sublimehq-pub.gpg -O /etc/apt/trusted.gpg.d/sublimehq-pub.gpg
+
+# Ensure apt is set up to work with https sources:
+
+sudo apt-get install apt-transport-https
+
+# Select the channel to use: Stable
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Update apt sources and install Sublime Text
+
+sudo apt update
+sudo apt install sublime-text
